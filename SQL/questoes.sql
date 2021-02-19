c2c72492b823c5c2b532481bc91d0b965ae1a890
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-02-19 10:44:30 -0300

    Estrutura básica do banco de dados de exemplo.

--- SQL/questoes.sql
+++ SQL/questoes.sql
@@ -1,1 +1,16 @@
 CREATE DATABASE luby;
+
+CREATE TABLE tabela_pessoa
+(
+	id int unsigned not null auto_increment,
+    nome varchar(50) not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tabela_evento
+(
+	id int unsigned not null auto_increment,
+    evento varchar(50) not null,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_evento_pessoa FOREIGN KEY (pessoa_id) REFERENCES tabela_pessoa (id)
+);
